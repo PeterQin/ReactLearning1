ce60cd4acd220a836c4701de7d3dd18e26e34a3a
Peter Qin <peter-qin@live.com>
2022-11-05 23:15:39 +0800

    Install ESLint and Prettier

--- my-app/src/Todo/Todo.tsx
+++ my-app/src/Todo/Todo.tsx
@@ -1,65 +1,56 @@
-import React, { ChangeEvent, ChangeEventHandler, FormEventHandler } from 'react';
-import TodoList from './TodoList';
+import React, { ChangeEventHandler, FormEventHandler } from 'react'
+import TodoList from './TodoList'
 
-type TodoAppProps = {
-}
+type TodoAppProps = Record<string, unknown>
 
 type TodoAppState = {
-  items: TodoItemProps[],
-  text: string,
+    items: TodoItemProps[]
+    text: string
 }
 
 export interface TodoItemProps {
-  text: string,
-  id: number,
+    text: string
+    id: number
 }
 
 class TodoApp extends React.Component<TodoAppProps, TodoAppState> {
-  
-    state: TodoAppState = { items: [], text: '' };
-  
+    state: TodoAppState = { items: [], text: '' }
     render() {
-      
-      return (
-        <div>
-          <h3>TODO</h3>
-          <TodoList items={this.state.items} />
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="new-todo">
-              What needs to be done?
-            </label>
-            <input
-              id="new-todo"
-              onChange={this.handleChange}
-              value={this.state.text}
-            />
-            <button>
-              Add #{this.state.items.length + 1}
-            </button>
-          </form>
-        </div>
-      );
+        return (
+            <div>
+                <h3>TODO</h3>
+                <TodoList items={this.state.items} />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-todo">What needs to be done?</label>
+                    <input
+                        id="new-todo"
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                    />
+                    <button>Add #{this.state.items.length + 1}</button>
+                </form>
+            </div>
+        )
     }
-  
+
     handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-      this.setState({ text: e.target.value });
+        this.setState({ text: e.target.value })
     }
-  
+
     handleSubmit: FormEventHandler = (e) => {
-      e.preventDefault();
-      if (this.state.text.length === 0) {
-        return;
-      }
-      const newItem = {
-        text: this.state.text,
-        id: Date.now()
-      };
-      this.setState(state => ({
-        items: state.items.concat(newItem),
-        text: ''
-      }));
+        e.preventDefault()
+        if (this.state.text.length === 0) {
+            return
+        }
+        const newItem = {
+            text: this.state.text,
+            id: Date.now(),
+        }
+        this.setState((state) => ({
+            items: state.items.concat(newItem),
+            text: '',
+        }))
     }
-  }
-  
-  export default TodoApp
- 
+}
+
+export default TodoApp
